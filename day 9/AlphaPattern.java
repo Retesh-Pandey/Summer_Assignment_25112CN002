889845e6cb8bf84344dec34b39af4057638c5a48
Retesh Kumar Pandey <reteshp751@gmail.com>
2026-06-05 10:08:47 +0530

    initial commit

---
+++ day 9/AlphaPattern.java
@@ -0,0 +1,14 @@
+public class AlphaPattern {
+    public static void main(String[]args)
+    {
+        char i,j;
+        for (i='A';i<='E';i++)
+        {
+            for (j='A';j<=i;j++)
+            {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
