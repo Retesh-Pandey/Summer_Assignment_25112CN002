560729d4251f4ed5ce1599e6da3b5ff4c7b16c62
Retesh Kumar Pandey <reteshp751@gmail.com>
2026-06-09 13:59:58 +0530

    initial commit

---
+++ day 13/CountEvenOdd.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class CountEvenOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        int evenCount = 0, oddCount = 0;
+
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+            if (arr[i] % 2 == 0) {
+                evenCount++;
+            } else {
+                oddCount++;
+            }
+        }
+
+        System.out.println("Even elements = " + evenCount);
+        System.out.println("Odd elements = " + oddCount);
+    }
+}
+
